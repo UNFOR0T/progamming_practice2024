8b2939cee6ae363beef19eb25e392e2ed3425fb7
UNFOR0T <167008859+UNFOR0T@users.noreply.github.com>
2024-09-12 10:23:05 +0900

    Add files via upload

---
+++ Practice2024-main/007_bmi/007_bmi.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	float weight, height;
+
+	printf("몸무게(kg) : ");
+	scanf_s("%f", &weight);
+	printf("키(cm) : ");
+	scanf_s("%f", &height);
+
+	height /= 100;	// m 단위로 변환
+
+	float bmi = weight / (height * height);
+
+	printf("몸무게 : %.1fkg\n", weight);
+	printf("키     : %.1fcm\n", height * 100);
+	printf("bmi    : %.1f\n", bmi);
+
+
+	
+}
